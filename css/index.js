af87ca58f4fe330d867a6febf9c195ccc6a344c1
zinaelnahel <zinamahmoud1989@gmail.com>
2021-02-22 16:03:55 +0100

    Adding an icon

--- css/index.js
+++ css/index.js
@@ -29,6 +29,16 @@ function displayWeather(response) {
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+   let humidityElement = document.querySelector("#humidity");
+   humidityElement.innerHTML = response.data.main.humidity;
+let windElement = document.querySelector("#wind");
+   windElement.innerHTML = Math.round(response.data.wind.speed);
+   let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searching(cityName) {
@@ -56,3 +66,4 @@ function getCurrentLocation(event) {
 
 let currentLocationBtn = document.querySelector("#btnLocation");
 currentLocationBtn.addEventListener("click", getCurrentLocation);
+
