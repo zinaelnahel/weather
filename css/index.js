10b384f2734da11566806326800c4e445f367935
zinaelnahel <zinamahmoud1989@gmail.com>
2021-02-26 22:08:10 +0100

    code clearning and correction of geolocation

--- css/index.js
+++ css/index.js
@@ -45,17 +45,17 @@ function displayWeather(response) {
   );
   let temperatureElement=document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#city");
-   let dateElement = document.querySelector("#date");
-   let humidityElement = document.querySelector("#humidity");
-let windElement = document.querySelector("#wind");
-   let iconElement = document.querySelector("#icon");
+  let dateElement = document.querySelector("#date");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
-   cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    humidityElement.innerHTML = response.data.main.humidity;
-     windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -109,7 +109,7 @@ function searchLocation(position) {
   let key = "2cea381e738c66dcff2a5021f253b186";
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}&units=metric`;
   axios.get(url).then(displayWeather);
-    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+url = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayDailyForecast);
 }
 
